c436cc6721af46e4fe40c52ae1313bb4dc06e72c
Sagar Tahelyani <sagar.ucet091@gmail.com>
2026-06-03 17:10:04 +0530

    - Changed the pipeline trigger branch - Upgrade the app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.quickmindgames.sudoku"
         minSdk = 28
         targetSdk = 36
-        versionCode = 9
-        versionName = "0.1.4"
+        versionCode = 10
+        versionName = "0.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
